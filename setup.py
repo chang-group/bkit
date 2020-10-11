c5dd23229ed2d48e3ae4e13cba9ec165a9753c72
Jeff Thompson <jpthompson17@gmail.com>
2020-10-10 19:21:20 -0700

    Added setup.py and bkit/__init__.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    version='0.1.0',
+    description='Binding Kinetics Toolkit',
+    url='https://github.com/chang-group/bkit',
+    author='Jeff Thompson',
+    author_email='jeffreyt@ucr.edu',
+    classifiers=[  
+        'Development Status :: 3 - Alpha',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    python_requires='>=3.6',
+    install_requires=['numpy', 'scipy', 'pyemma', 'networkx', 'mdtraj', 'matplotlib']
+)
+
