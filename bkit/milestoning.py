ffaacc398df47c9b3195383bb5570d66d2282bd0
Jeff Thompson <jpthompson17@gmail.com>
2020-10-27 23:56:11 -0700

    Reorganize milestoning module.

--- bkit/milestoning.py
+++ bkit/milestoning.py
@@ -1,168 +1,196 @@
-import msmtools.analysis as analysis
+import bkit.markov as markov
 import networkx as nx
 import numpy as np
 import scipy.spatial as spatial
-import scipy.stats as stats
-from _schedule import Schedule
+from bkit._schedule import Schedule
 
 
-class MilestoningModel:
-    """Milestoning model with given rate matrix"""
+class MarkovianMilestoningModel(markov.ContinuousTimeChain):
+    """Milestoning process governed by a continuous-time Markov chain."""
 
-    def __init__(self, rate_matrix):
-        """Initialize milestoning model.
+    def __init__(self, rate_matrix, milestones):
+        """Construct the model from a rate matrix and a set of milestones.
 
         Parameters
         ----------
-        rate_matrix : ndarray (m, m)
+        rate_matrix : ndarray (M, M)
+            Matrix :math:`Q` of transition rates between milestones.
+        milestones : list of length M
+            The milestones corresponding to the indices of :math:`Q`.
 
         """
-
-        assert analysis.is_rate_matrix(rate_matrix)
-        self._Q = rate_matrix
-        self._t = -1 / np.diag(self._Q)
-        self._P = np.fill_diagonal(self._Q * self._t[:, np.newaxis], 0)
-        self._q = analysis.stationary_distribution(self._P)
-        self._p = self._q * self._t
-        self._p /= sum(self._p)
+        super().__init__(rate_matrix)
+        self.milestones = milestones
 
     @property
-    def rate_matrix(self):
-        return self._Q
+    def milestones(self):
+        """The milestones in indexed order."""
+        return self._milestones
+
+    @milestones.setter
+    def milestones(self, value):
+        if len(value) != len(self.states):
+            raise ValueError('Length does not match number of milestones.')
+        self._milestones = milestones
 
     @property
-    def equilibrium_populations(self):
-        return self._p
-
-    @property
-    def transition_matrix(self):
-        return self._P
-
-    @property
-    def stationary_fluxes(self):
-        return self._q
+    def transition_kernel(self):
+        """Transition probability matrix :math:`K`."""
+        return self.jump_chain.transition_matrix
 
     @property
     def mean_lifetimes(self):
-        return self._t
-    
-    def mfpts(self, target):
-        """Mean first passage times to a set of target milestones
+        """The mean waiting time associated with each milestone.""" 
+        return -1. / np.diag(self.rate_matrix)
+
+    @property
+    def stationary_fluxes(self):
+        """The stationary flux vector :math:`q`."""
+        return self.jump_chain.stationary_distribution
+
+
+class MilestoningEstimator:
+
+    def __init__(self, milestones):
+        """Initialize estimator.
 
         Parameters
         ----------
-        target : int or list of int
-            indices of target milestones (sinks)
-
-        Returns
-        -------
-        mfpts : ndarray (m,)
-            mean first passage times to target set
+        milestones : list
+            Milestones on which trajectories are decomposed.
 
         """
+        self._ix = dict((a, ix) for ix, a in enumerate(milestones))
+        self._schedules = []
+        self._count_matrix = np.zeros((len(milestones), len(milestones),
+                                      dtype=int)
+        self._total_time = dict((a, 0) for a in milestones)
 
-        P = np.copy(self._P)
-        P[target, :] = 0
-        return np.linalg.solve(np.fill_diagonal(-P, 1), self._t)
+    def load_schedules(self, schedules):
+        """Incorporate data in the form of milestone schedules.
 
+        Parameters
+        ----------
+        schedules : list of Schedule
+            Sequences of (milestone, lifetime) pairs obtained by 
+            trajectory decomposition.
 
-class MilestoningMLEstimator:
-    """Maximum likelihood milestoning estimator"""
+        """
+        for schedule in schedules:
+            it = zip(schedule[:-1], schedule[1:])
+            for (a, t), (b, _) in it:
+                if a not in self._ix or b not in self._ix:
+                    continue
+                self._count_matrix[self._ix[a], self._ix[b]] += 1
+                self._total_time[a] += t
+        self._schedules += schedules
+
+    @property
+    def max_likelihood_markov_model(self):
+        """The maximum likelihood MarkovianMilestoningModel."""
+        pass
+        
+
+class TrajectoryDecomposer:
+    """Decomposition of trajectories by milestoning."""
 
     def __init__(self, anchors, cutoff=np.inf, boxsize=None):
-        """Initialize milestoning estimator.
+        """Define milestones by Voronoi tessellation.
 
         Parameters
         ----------
         anchors : ndarray (N, d) or list of ndarray (N_i, d)
             Generating points for Voronoi tessellation. 
-            If a list of ndarrays is given, each group of anchors
-            corresponds to a union of Voronoi cells that should be
-            treated as a single cell.
+            If a list of ndarrays is given, each subset of anchors
+            indicates a union of Voronoi cells that should be treated 
+            as a single cell.
 
         cutoff : positive float, optional
-            Maximum distance to nearest anchor. The region beyond
-            this cutoff is treated as an absorbing (cemetery) state.
-
-        boxsize : array_like or scalar, optional (not yet implemented)
+            Maximum distance to nearest anchor. A trajectory segment
+            between two milestones is only counted as a transition if 
+            it stays within the cutoff.
+ 
+       boxsize : array_like or scalar, optional (not yet implemented)
             Apply d-dimensional toroidal topology (periodic boundaries)
 
         """
-
         if type(anchors) is np.ndarray:
-            nanchors, ndim = anchors.shape
-            self._parent_cell = list(range(nanchors))
+            self._parent_cell = dict((i, i) for i in range(len(anchors))
         else:
-            ndim = anchors[0].shape[1]
-            assert all(a.shape[1] == ndim for a in anchors[1:])
-            self._parent_cell = [i for i, a in enumerate(anchors) for x in a]
+            self._parent_cell = dict((k, i) for i, a in enumerate(anchors)
+                                            for k in range(len(a))]
             anchors = np.concatenate(anchors)
+        n_anchors, n_dim = anchors.shape 
 
         G = nx.Graph()
-        if ndim > 1:
+        if n_dim > 1:
             tri = spatial.Delaunay(anchors)
             indptr, indices = tri.vertex_neighbor_vertices
-            G.add_edges_from([(k, l) for k in range(nanchors-1) 
+            G.add_edges_from([(k, l) for k in range(n_anchors-1) 
                               for l in indices[indptr[k]:indptr[k+1]]])
         else:
-            G.add_edges_from([(k, k+1) for k in range(nanchors-1)])
-        partition = lambda k, l: self._parent_cell[k] == self.parent_cell[l]
-        self._graph = nx.quotient_graph(G, partition, relabel=True)
+            G.add_edges_from([(k, k+1) for k in range(n_anchors-1)])
+        partition = lambda k, l: self._parent_cell[k] == self._parent_cell[l]
+        self._graph = nx.quotient_graph(G, partition)
 
-        self._anchor_kdtree = spatial.cKDTree(anchors)    
+        self._kdtree = spatial.cKDTree(anchors)    
+        
         self._cutoff = cutoff
         if np.isfinite(cutoff):
             self._parent_cell.append(None)
-
-        self._dtrajs = []
-        self._dts = []
-        self._schedules = []
     
     @property
+    def anchors(self):
+        """Generating points of the Voronoi tessellation."""
+        return self._kdtree.data
+
+    @property
+    def cells(self):
+        """Cells of the partition (frozensets of anchor indices).""" 
+        return self._graph.nodes
+
+    @property
     def milestones(self):
-        return [set(e) for e in self._graph.edges]  
+        """Milestones (pairs of adjacent cells)."""
+        return self._graph.edges  
  
-    def load_trajectory_data(self, trajs, dt=1.):        
-        for traj in trajs:
-            _, indices = self._anchor_kdtree.query(traj, distance_upper_bound=self._cutoff)
-            dtraj = np.fromiter((self._parent_cell[k] for k in indices), int)
-            self._dtrajs.append(dtraj)
-            self._dts.append(dt)
-            self._schedules.append(_milestone_schedule(dtraj, dt))
+    def decompose(self, trajs, dt=1.):
+        """Decompose trajectories according to last-hit milestone.
+
+        Parameters
+        ----------
+        trajs : ndarray (T, d) or list of ndarray (T_i, d)
+            Trajectories to be milestoned.
+
+        dt : positive float, optional
+            Trajectory sampling interval.
+
+        Returns
+        -------
+        schedules : Schedule or list of Schedule
+            Sequences of (milestone, lifetime) pairs obtained by 
+            trajectory decomposition. The initial milestone of a
+            trajectory is defined to be {None, initial_cell}.
+
+        """ 
+        if type(trajs) is np.ndarray:
+            return self._milestone_schedule(traj, dt)
+        return [self._milestone_schedule(traj, dt) for traj in trajs]
+
+    def _milestone_schedule(self, traj, dt):
+        _, indices = self._kdtree.query(traj, distance_upper_bound=self._cutoff)
+        dtraj = np.fromiter((self._parent_cell[k] for k in indices), int)
+        return _dtraj_to_milestone_schedule(dtraj, dt)
  
     def remove_milestone(self, i, j):
         assert self._graph.has_edge(i, j)
         self._graph = nx.contracted_nodes(self._graph, i, j, self_loops=False)
         self._parent_cell = [i if k == j else k for k in self._parent_cell]
-        for q, dtraj in enumerate(self._dtrajs):
-            self._dtrajs[q] = [i if k == j else k for k in dtraj]
-            self._schedules[q] = _milestone_schedule(dtraj, self._dts[q])
-    
-    def estimate(self):
-        # TODO: Check for jumps between non-adjacent cells.
-
-        M = self._graph.number_of_edges()
-        self._count_matrix = np.zeros((M, M), dtype=np.int32)
-        self._lifetimes = [[] for m in range(M)]
-
-        index = dict((frozenset(e), m) 
-                     for m, e in enumerate(self._graph.edges))
-        for schedule in self._schedules:
-            it = zip(schedule[:-1], schedule[1:])
-            for (source, lifetime), (target, _) in enumerate(it):
-                if source in index and target in index:
-                    self._count_matrix[index[source], index[target]] += 1
-                    self._lifetimes[index[source]].append(lifetime)
- 
-        self.K = self._count_matrix / np.sum(self._count_matrix, 
-                                             axis=1)[:, np.newaxis]
-        self.t = np.fromiter(map(np.mean, self._lifetimes), float)
-        self.t_stderr = np.fromiter(map(stats.sem, self._lifetimes), float)
 
 
-def _milestone_schedule(dtraj, dt=1.):
+def _dtraj_to_milestone_schedule(dtraj, dt=1.):
     initial_milestone = frozenset({None, dtraj[0]})
-    schedule = Schedule([initial_milestone], [0])
+    schedule = Schedule([initial_milestone], [0.])
     for i, j in zip(dtraj[:-1], dtraj[1:]):
         if j in schedule.labels[-1]:
             schedule.lengths[-1] += dt
