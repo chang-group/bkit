5405b7f5a664fccd4e44a85e878e508eeeaf78bb
Jeff Thompson <jpthompson17@gmail.com>
2020-11-03 12:49:53 -0800

    Fix typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='bkit'
+    name='bkit',
     version='0.1.0',
     description='Binding Kinetics Toolkit',
     url='https://github.com/chang-group/bkit',
