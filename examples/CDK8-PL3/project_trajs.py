39b5f52da1b5e6f7b0b338a6129eecf0d2b120ec
Jeff Thompson <jpthompson17@gmail.com>
2020-12-11 23:23:34 -0800

    Handle default arguments

--- examples/CDK8-PL3/project_trajs.py
+++ examples/CDK8-PL3/project_trajs.py
@@ -7,22 +7,18 @@ import pyemma
 import sys
 
 
-parser = argparse.ArgumentParser()
-
-d = 'pca.h5'
-h = ('HDF5 file containing a saved PyEMMA PCA or TICA object equipped with '
-     + f'a FeatureReader (default: {d})')
-parser.add_argument('--model', metavar='MODELFILE', default=d, help=h)
-
-d = 'ptrajs.npz'
-h = f'NumPy .npz file containing the projected trajectories (default: {d})'
-parser.add_argument('--output', metavar='OUTPUTFILE', default=d, help=h)
-
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+h = ('HDF5 file containing a saved PyEMMA PCA or TICA object '
+     + 'equipped with a FeatureReader')
+parser.add_argument('--model', metavar='MODELFILE', 
+                    default='pca.h5', help=h)
+h = 'NumPy .npz file containing the projected trajectories'
+parser.add_argument('--output', metavar='OUTPUTFILE', 
+                    default='ptrajs.npz', help=h)
 h = ('trajectory file compatible with the FeatureReader of the '
      + 'transformation object saved in MODELFILE')
-parser.add_argument('trajfiles', metavar='TRAJFILE', 
-                    type=str, nargs='+', help=h)
-
+parser.add_argument('trajfiles', metavar='TRAJFILE', nargs='+', help=h)
 args = parser.parse_args()
 
 transformer = pyemma.load(args.model)
