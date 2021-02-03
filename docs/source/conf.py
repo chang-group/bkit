3f7e384e9d1120bdb95415c53737c29702b6d619
Jeff Thompson <jpthompson17@gmail.com>
2021-02-02 21:21:17 -0800

    Update conf.py. Remove mock imports.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,18 +9,6 @@ import sys
 
 sys.path.insert(0, os.path.abspath('../..'))
 
-# Mock out imports
-
-from unittest.mock import MagicMock
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-            return Mock()
-
-MOCK_MODULES = ['msmtools', 'numpy', 'scipy.spatial']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-
 # -- Project information -----------------------------------------------------
 
 project = 'BKiT'
